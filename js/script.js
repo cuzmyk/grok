44903d302c781edc8caf7faa0bb7567050512dbb
cuzmyk <mixon749@gmail.com>
2023-01-14 18:42:33 +0300

    fist animations commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,66 @@
-const body = document.querySelector("body");
-// console.log(body);
+// Найти все ссылки начинающиеся на #
+const anchors = document.querySelectorAll('a[href^="#"]');
 
-window.addEventListener("scroll", () => {
-  body.style.backgroundPosition = +window.pageYOffset + "px";
-});
+// Цикл по всем ссылкам
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault(); // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute("href")
+      ? anchor.getAttribute("href")
+      : "body";
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+
+const animItems = document.querySelectorAll(".animated_item");
+console.log(animItems);
+
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        window.scrollY > animItemOffset - animItemPoint &&
+        window.scrollY < animItemOffset + animItemPoint
+      ) {
+        animItem.classList.add("animated");
+      } else {
+        animItem.classList.remove("animated");
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+      scrollTop = window.scrollY || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+  }
+}
+
+setTimeout(() => {
+  animOnScroll();
+}, 300);
+
+// const bgImgs = document.querySelector(".bg__img");
+
+// window.addEventListener("scroll", function () {
+//   console.log(window.scrollY);
+//   if (window.scrollY > 600) {
+//     bgImgs.style.left = `${0 - (window.scrollY - 600) / 100}em`;
+//   }
+// });
